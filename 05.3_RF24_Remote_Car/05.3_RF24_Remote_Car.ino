0cb2aadf54ac4a9be08486fc63400c0b9f40a602
Tobias Bosch <tbosch@google.com>
2021-12-31 07:41:57 +0100

    Measure every sensor individually using an interrupt to be more precise.

--- 05.3_RF24_Remote_Car/05.3_RF24_Remote_Car.ino
+++ 05.3_RF24_Remote_Car/05.3_RF24_Remote_Car.ino
@@ -10,6 +10,7 @@
 #include "Freenove_4WD_Car_for_Arduino.h"
 #include "RF24_Remote.h"
 
+#define DEBUG 0
 #define NRF_UPDATE_TIMEOUT    1000
 
 #define MCP23017_ADDRESS 0x27
@@ -20,27 +21,29 @@
 #define MCP23017_IODIRB 0x10
 #define MCP23017_GPIOA 0x09
 #define MCP23017_GPIOB 0x19
+#define MCP23017_INTCONB 0x14
+#define MCP23017_DEFVALB 0x13
+#define MCP23017_GPINTENB 0x12
+#define MCP23017_INTFB 0x17
+#define MCP23017_INTCAPB 0x18
 
-// Note: This can't be higher than BUFFER_LENGTH from Wire.h!
-#define SONIC_READ_COUNT 32
-// 10ms = 3.3m of sound.
+#define MCP23017_INT_ARDUINO_PIN 2
+
+// 6ms = approx. 2m of sound.
 // Don't set this too high so the loop is not blocked for too long.
-#define SONIC_READ_TIME_US 10000
+#define SONIC_READ_TIME_US 6000
 #define SONIC_SPEED_CM_PER_US 0.033
 
 u32 lastNrfUpdateTime = 0;
 u32 lastBeepUpdateTime = 0;
 float sonicResolution = 0;
-float sonicMinDistance = 0;
-float sonicDistances[8] = {0};
+volatile long sonicInterruptTime = 0;
 
 void setup() {
-  Serial.begin(9600);  
+  if (DEBUG) {
+    Serial.begin(9600);  
+  }
   Wire.begin();
-  // Note: This doesn't work with the LED strip anymore :-(
-  // We set this to get a higher accuracy from the sonic sensors.
-  // (from about 250us down to 120us per read loop).
-  Wire.setClock(400000);
   
   pinsSetup();
   if (!nrf24L01Setup()) {
@@ -50,42 +53,39 @@ void setup() {
   initSonic();
 }
 
-// TODO: Show sonic distances on the client display as top down view.
 // TODO: Also read the location sensor too!
 // -> show a compass on the client display!
-// TODO: Get slower the closer we get to a wall
-// TODO: Allow to continue driving when one of the buttons is pressed.
-// 
-void loop() {  
-  readSonicSensor();
+// TODO: Enable the LED strip again too!
+void loop() {
+  long measureStartTime = micros();  
   for (int i=0; i<8; ++i) {
-    nrfDataWrite[i] = sonicDistances[i];
+    float distance = readSonicSensor(i);
+    if (distance > 0) {
+      nrfDataWrite[i] = readSonicSensor(i);
+    }
   }
-  if (/*debug=*/false) {
-    Serial.print(">>> min distance in cm:");
-    Serial.print(sonicMinDistance);
-    Serial.print(" resolution in cm:");
-    Serial.print(sonicResolution);
-    Serial.println();
-  }
-  if (/*debug=*/false) {
-    Serial.print(">>> distances in cm:");
-    for (int b=0; b<8; ++b) {
-      Serial.print(sonicDistances[b]);
+  long measureDuration = micros() - measureStartTime;
+  if (DEBUG) {
+    Serial.print(" measure time:");
+    Serial.print(measureDuration / 1000);
+    Serial.print(" distances in cm:");
+    for (int i=0; i<8; ++i) {
+      Serial.print(nrfDataWrite[i]);
       Serial.print(" ");
     }
     Serial.println();
   }
 
-  if (sonicMinDistance < 100){
-    setBuzzer(true);
-    if( nrfDataRead[7] == 1) {
-      motorRun(0, 0);
-      return;
-    }
-  } else {
-    setBuzzer(false);   
-  }
+  // TODO: Do this on the remote!
+//  if (sonicMinDistance < 100){
+//    setBuzzer(true);
+//    if( nrfDataRead[7] == 1) {
+//      motorRun(0, 0);
+//      return;
+//    }
+//  } else {
+//    setBuzzer(false);   
+//  }
  
   
   if (getNrf24L01Data()) {
@@ -100,6 +100,10 @@ void loop() {
   }  
 }
 
+void sonicInterrupt() {
+  sonicInterruptTime = micros();
+}
+
 void initSonic() {
   // Set BANK bit = Enable 8 bit mode. This allows us to read a single bank,
   // which is faster.
@@ -112,47 +116,55 @@ void initSonic() {
   writeIOExpansionRegister(MCP23017_IODIRA,0x00);
   // Mark all B pins as input.
   writeIOExpansionRegister(MCP23017_IODIRB,0xff);  
+
+  // Setup to trigger interrupt on falling value.
+  // set the pin interrupt control:
+  // 1 = compare against given value.
+  writeIOExpansionRegister(MCP23017_INTCONB,0xff);  
+  // Set default value to 1, different value (=0) triggers interrupt.
+  writeIOExpansionRegister(MCP23017_DEFVALB,0xff);
+  // Setup a callback for the arduino INT handler.
+  pinMode(MCP23017_INT_ARDUINO_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(MCP23017_INT_ARDUINO_PIN),sonicInterrupt,FALLING);
 }
 
-void readSonicSensor() {
+float readSonicSensor(int idx) {
+  if (idx<0 || idx >7) {
+    return 0;
+  }
+  // Enable interrupts only for that sensor.
+  writeIOExpansionRegister(MCP23017_GPINTENB,1<<idx);
+
   // make trigPin output high level lasting for 10μs to triger HC_SR04
-  writeIOExpansionRegister(MCP23017_GPIOA, 0xFF);
+  // Note: Somehow I wired the trigger and sensor pins in the opposite directions!
+  // -> 0x80 trigger on bank A = 0x01 sensor on bank B.
+  writeIOExpansionRegister(MCP23017_GPIOA, 1<<(7-idx));
   delayMicroseconds(10);
   writeIOExpansionRegister(MCP23017_GPIOA, 0x00);
-  
+
+  // Wait for the high value.
+  // This is also clearing the interrupt value so we can receive further interrupts.  
+  long startTime = 0;
   Wire.beginTransmission(MCP23017_ADDRESS);
   Wire.write(MCP23017_GPIOB);
   Wire.endTransmission();
-  long sonicStartTimes[8] = {0};
-  long sonicEndTimes[8] = {0};
-  long currTime = micros();
-  long loopStartTime = micros();
-  long loopCount = 0;
-  for (long currTime = loopStartTime; currTime - loopStartTime < SONIC_READ_TIME_US; currTime = micros(), ++loopCount) {
-    Wire.requestFrom(MCP23017_ADDRESS, 1, /*sendStop=*/false);
-    uint8_t data = Wire.read();
-    for (int b=0; b<8; ++b) {
-      bool isSet = data & (1 << b);
-      if (sonicStartTimes[b] == 0 && isSet) {
-        sonicStartTimes[b] = currTime;
-      } else if (sonicStartTimes[b] > 0 && sonicEndTimes[b] == 0 && !isSet) {
-        sonicEndTimes[b] = currTime;
-      }
+  for (int i=0; i<10 && startTime == 0; i++) {
+    Wire.requestFrom(MCP23017_ADDRESS, 1, /*sendStop=*/false);  
+    if (Wire.read() > 0) {
+      startTime = micros();
     }
   }
-  sonicResolution = (micros() - loopStartTime) * SONIC_SPEED_CM_PER_US / loopCount;
+  if (startTime == 0) {
+    // We were too slow to capture the pulse.
+    return 0;
+  }
+  sonicInterruptTime = 0;
+  delayMicroseconds(SONIC_READ_TIME_US);
   
-  sonicMinDistance = SONIC_READ_TIME_US * SONIC_SPEED_CM_PER_US;
-  for (int b=0; b<8; ++b) {
-    long duration = SONIC_READ_TIME_US;
-    if (sonicEndTimes[b] > 0) {
-      duration = sonicEndTimes[b] - sonicStartTimes[b];
-    }
-    float distanceCm = duration * SONIC_SPEED_CM_PER_US;     
-    // Average over the last 3 runs to reduce flakiness.
-    sonicDistances[b] = (sonicDistances[b]*2 + distanceCm) / 3;    
-    sonicMinDistance = min(sonicMinDistance, sonicDistances[b]);
-  }
+  if (sonicInterruptTime == 0) {
+    return SONIC_READ_TIME_US * SONIC_SPEED_CM_PER_US;
+  }    
+  return (sonicInterruptTime - startTime) * SONIC_SPEED_CM_PER_US;
 }
 
 void writeIOExpansionRegister(uint8_t regAddr, uint8_t regValue){
